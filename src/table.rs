455ca69d56b2c89bfa3da34a1cacab348b4c4a5c
tatarize <tatarize@gmail.com>
2024-01-15 19:12:45 -0800

    Add spacemask even-odd

--- src/table.rs
+++ src/table.rs
@@ -1,4 +1,17 @@
-use crate::geometry::Point;
+use vsvg::exports::egui::Shape::Vec;
+use crate::geometry::{Geomstr, Point};
+
+pub struct SpaceMask {
+    pub inside: Vec<Vec<bool>>,
+}
+
+impl SpaceMask {
+    pub fn new(mask: Vec<Vec<bool>>) -> SpaceMask {
+        SpaceMask {
+            inside: mask,
+        }
+    }
+}
 
 pub struct BeamTable {
     pub events: Vec<Point>,
@@ -11,6 +24,24 @@ impl BeamTable {
             events: Vec::new(),
             actives: Vec::new(),
         }
+    }
+
+    pub fn evenodd_fill(&self, geometry: Geomstr, settings: f64) -> SpaceMask {
+        let spacemask = Vec::new();
+        for active in &self.actives {
+            let active_mask = Vec::new();
+            let mut inside = false;
+            active_mask.push(inside);
+            for a in active {
+                let line = &geometry.segments[a];
+                if line.2.1 == settings {
+                    inside = !inside;
+                }
+                active_mask.push(inside);
+            }
+            spacemask.push(active_mask);
+        }
+        SpaceMask::new(spacemask)
     }
 
     pub fn actives_at(&self, x: f64, y: f64) -> &Vec<i32> {
