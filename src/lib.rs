60e8dd84f9a5283b3c465339485da2f80d60fd63
tatarize <tatarize@gmail.com>
2024-01-17 01:57:33 -0800

    Create pure version of beamtable (python binding)

--- src/lib.rs
+++ src/lib.rs
@@ -6,56 +6,20 @@ mod tests;
 use crate::geometry::Geomstr;
 use crate::table::BeamTable;
 use pyo3::prelude::*;
-// #[pyclass]
-// struct BeamTable {
-//     #[pyo3(get)]
-//     pub events: Vec<PyComplex>,
-//     #[pyo3(get)]
-//     pub actives: Vec<Vec<i32>>,
-// }
-//
-// #[pymethods]
-// impl BeamTable {
-//     #[new]
-//     pub fn new() -> BeamTable {
-//         BeamTable {
-//             events: Vec::new(),
-//             actives: Vec::new(),
-//         }
-//     }
-// }
 
-///
-// #[pyfunction]
-// fn build(segments: Vec<(f64, f64, f64, f64)>) -> PyResult<(Vec<(f64, f64)>, Vec<Vec<i32>>)> {
-//     let mut table = ScanBeam::from_floats(segments);
-//     let q = table.build();
-//
-//     let mut segs = Vec::new();
-//     for m in q.events.iter() {
-//         segs.push((m.x, m.y));
-//     }
-//     Ok((segs, q.actives))
-// }
 #[pyfunction]
-fn build(
+fn union(
     segments: Vec<((f64, f64), (f64, f64), (f64, f64), (f64, f64), (f64, f64))>,
-) -> (Vec<(f64, f64)>, Vec<Vec<i32>>) {
+) -> Vec<((f64, f64), (f64, f64), (f64, f64), (f64, f64), (f64, f64))> {
     let mut table = BeamTable::new(Geomstr::from_segments(segments));
     table.build();
-
-    let mut segs = Vec::new();
-    for m in table.events.iter() {
-        segs.push((m.x, m.y));
-    }
-    (segs, table.actives)
+    let bo = table.union_all();
+    table.create(bo).segments
 }
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn beamtable(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(build, m)?)?;
-    // m.add_function(wrap_pyfunction!(complex_test, m)?)?;
-    // m.add_class::<BeamTable>()?;
+    m.add_function(wrap_pyfunction!(union, m)?)?;
     Ok(())
 }
